6acbea4e2f57874bb2191ac1767a54a977649ef4
Deviyuniarti <deviyuni866@gmail.com>
2023-10-20 12:22:34 +0700

    push pertemuan3

---
+++ rest-api/app/Http/Controllers/AnimalController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
+{
+    public $animals = ['Beruang', 'Bebek'];
+    public function index(){ 
+        echo "Menampilkan data animals <br>";
+
+        //loop property animals
+        foreach($this->animals as $animal)
+        echo "-$animal <br>";
+    }
+
+    public function store(request $request){
+        echo "Menambahkan hewan baru <br>";
+
+        array_push($this->animals, $request->animal);
+
+        //panggil method index
+        $this->index();
+    }
+
+    public function update($id, Request $request){
+        echo "Mengupdate data hewan id $id. <br>";
+
+        // update data di property animals
+        $this->animals[$id] = $request->animal;
+
+        // panggil method index
+        $this->index();
+    }
+
+    public function destroy($id){
+        echo "Menghapus data hewan id $id. <br>";
+
+        unset($this->animals[$id]);
+
+        $this->index();
+    }
+}
